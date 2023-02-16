910f780b0106f3f8d13b1c97a06b732e93f59685
Dmitri Gvozdev <dmitrigvozdev94@gmail.com>
2023-02-16 18:03:54 +0000

    Tags

--- main.tf
+++ main.tf
@@ -131,8 +131,8 @@ resource "azurerm_linux_virtual_machine" "catapp" {
   }
 
   tags = {
-    Department = "Something"
-    Billable = "Me"
+    Department = "devops"
+    Billable = "true"
   }
 
   # Added to allow destroy to work correctly.
